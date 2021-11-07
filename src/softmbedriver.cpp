d854680252b0da3bf6b245ef1f248c08f84c4b17
The Knatterfunkers <knatterfunker@protonmail.com>
2021-11-07 12:38:38 +0100

    Parsing codecserver config for uvquaulity and initiating driver accordingly

--- src/softmbedriver.cpp
+++ src/softmbedriver.cpp
@@ -10,7 +10,22 @@ std::string Driver::getIdentifier()
 
 Device* Driver::buildFromConfiguration(std::map<std::string, std::string> config)
 {
-    return new Device(3);
+    if (config.find("uvquality") == config.end())
+    {
+        std::cerr << "Using default uvquality of 3 for softmbe" << std::endl;
+        return new Device(3);
+    }
+
+    try
+    {
+        int uvquality = stoi(config["uvquality"]);
+        std::cerr << "Using uvquality of " << uvquality << " for softmbe" << std::endl;
+        return new Device(uvquality);
+    } catch (std::invalid_argument)
+    {
+        std::cerr << "Unable to process uvquality. Using default of 3 for softmbe" << std::endl;
+        return new Device(3);
+    }
 }
 
 static int registration = CodecServer::Registry::registerDriver(new Driver());
